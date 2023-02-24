349ae47c45d7d42d9c694edb3ebf54a40d90e1a5
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-02-24 20:36:08 +0100

    end

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script></script>
+
+<template>
+  <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style scoped>
+h1 {
+  background-color: #555;
+  color: coral;
+  margin: 0;
+}
+</style>
---
+++ src/components/jumbotron.vue
@@ -0,0 +1,12 @@
+<script></script>
+
+<template>
+  <div class="jumbotron"></div>
+</template>
+
+<style scoped>
+.jumbotron {
+  background-color: coral;
+  height: 100px;
+}
+</style>
